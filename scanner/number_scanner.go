ccfbc7662faaae87ed9ca239c92426ce129c0565
ruohone <2621185294@qq.com>
2022-05-03 10:36:10 +0800

    complete scan formula.

--- scanner/number_scanner.go
+++ scanner/number_scanner.go
@@ -25,8 +25,6 @@ func (ns NumberScanner) Scan(formula []rune, startPos int) (_ *token.TokenValue,
 		}, startPos
 	}
 
-	float64Value := &token.Float64Value{}
-
 	var runeVal []rune
 	for ; startPos < len(formula); startPos++ {
 		legal := ns.Of(formula[startPos])
@@ -36,11 +34,11 @@ func (ns NumberScanner) Scan(formula []rune, startPos int) (_ *token.TokenValue,
 		runeVal = append(runeVal, formula[startPos])
 	}
 
-	float64Value.Val, _ = strconv.ParseFloat(string(runeVal), 64)
+	val, _ := strconv.ParseFloat(string(runeVal), 64)
 
 	return &token.TokenValue{
 		Tok:   token.Number,
-		Value: float64Value.Val,
+		Value: val,
 	}, startPos
 }
 
