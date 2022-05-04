dc5b51353d95afbb98c99c47e88bf87c11946ecb
ruohone <2621185294@qq.com>
2022-05-04 21:40:01 +0800

    good test.

--- example/costumize_test.go
+++ example/costumize_test.go
@@ -12,7 +12,7 @@ import (
 )
 
 func TestConsumized(t *testing.T) {
-	scanner := scanner.NewScanner("A2(5)", scanner.WithAddScanners(
+	scanner := scanner.NewScanner("A2(5)*5(A+A)", scanner.WithAddScanners(
 		scanner.NumberScanner{}, scanner.SeparatorScanner{}, scanner.OperatorScanner{},
 		costumize.MyScanner{CastMap: costumize.CastMap}))
 
