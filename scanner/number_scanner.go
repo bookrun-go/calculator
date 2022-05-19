aed17ffc8a28cedd623eba2a9246747bca68cf3c
ruohone <2621185294@qq.com>
2022-05-19 13:00:48 +0800

    suppert point number.

--- scanner/number_scanner.go
+++ scanner/number_scanner.go
@@ -27,12 +27,24 @@ func (ns NumberScanner) Scan(formula []rune, startPos int) (_ *token.TokenValue,
 	}
 
 	var runeVal []rune
+	var pointExist bool
 	for ; startPos < len(formula); startPos++ {
+		if ns.isPoint(formula[startPos]) { // 检查小数点是否重复
+			if pointExist {
+				break
+			} else {
+				pointExist = true
+			}
+		}
 		legal := ns.of(formula[startPos])
 		if !legal {
 			break
 		}
 		runeVal = append(runeVal, formula[startPos])
+	}
+	if ns.isPoint(runeVal[len(runeVal)-1]) {
+		startPos--
+		runeVal = runeVal[:len(runeVal)-1]
 	}
 
 	val, _ := strconv.ParseFloat(string(runeVal), 64)
@@ -45,6 +57,10 @@ func (ns NumberScanner) Scan(formula []rune, startPos int) (_ *token.TokenValue,
 
 func (NumberScanner) Precedence() int {
 	return 1
+}
+
+func (NumberScanner) isPoint(char rune) bool {
+	return char == NumberPoint
 }
 
 func (NumberScanner) of(char rune) bool {
